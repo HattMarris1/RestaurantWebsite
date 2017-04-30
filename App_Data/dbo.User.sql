dd1a384191034b7ca5fa5ddd05a59015f25feba8
Matthew Harris <m.c.harris@hotmail.co.uk>
2017-04-30 15:04:38 +0100

    added the rest of the database tables

---
+++ App_Data/dbo.User.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[User] (
+    [Id]       INT          NOT NULL,
+    [UserName] VARCHAR (20) NULL,
+    [Password] VARCHAR (20) NULL,
+    [IsAdmin]  BIT          NULL,
+    [FavFood]  VARCHAR (15) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+

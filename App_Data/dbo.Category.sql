bb4dbb379a151442f13af514693556a84e1f82dd
Matthew Harris <m.c.harris@hotmail.co.uk>
2017-04-30 14:12:25 +0100

    added category and MenuItem tables

---
+++ App_Data/dbo.Category.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Category]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [UserName] VARCHAR(20) NULL, 
+    [Password] VARCHAR(20) NULL, 
+    [IsAdmin] BIT NULL, 
+    [FavFood] VARCHAR(15) NULL
+)
---
+++ App_Data/dbo.MenuItem.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[MenuItem]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[Name] Varchar(20) NOT NULL, 
+    [IsVeg] BIT NULL DEFAULT 0, 
+    [IsNew] BIT NULL DEFAULT 0, 
+    [Description] VARCHAR(50) NULL, 
+    [Category] INT NULL,
+
+)
